d3f3743e182c8137dee1a461b16582fd483a1002
Gleb <glebslivko777@gmail.com>
2020-03-17 16:16:26 +0200

    add new dist

---
+++ simple-blog/dist/precache-manifest.a9de41165ed4097f766a84ad235d3aa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ec5cac56857880d115d47977c6e290",
+    "url": "/DevelopsToday/simple-blog/dist/index.html"
+  },
+  {
+    "revision": "a1bf836fe858d97902df",
+    "url": "/DevelopsToday/simple-blog/dist/static/css/main.08b5e105.chunk.css"
+  },
+  {
+    "revision": "79d121a0a2a120f4a502",
+    "url": "/DevelopsToday/simple-blog/dist/static/js/2.f86cbe30.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/DevelopsToday/simple-blog/dist/static/js/2.f86cbe30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1bf836fe858d97902df",
+    "url": "/DevelopsToday/simple-blog/dist/static/js/main.a6308bd0.chunk.js"
+  },
+  {
+    "revision": "016a2ccf952fa205880a",
+    "url": "/DevelopsToday/simple-blog/dist/static/js/runtime-main.c0c4f9af.js"
+  }
+]);
