ebc031e12f69b8955d95bf1770766f9631e6036b
Gleb <glebslivko777@gmail.com>
2020-03-17 15:50:29 +0200

    add dist folder

---
+++ simple-blog/dist/precache-manifest.09b249ce678dc76b0b76235a8ba72259.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984d34c0a5e58a134b61caefdac90f24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8e805d531cc4cc85c55",
+    "url": "/static/css/main.08b5e105.chunk.css"
+  },
+  {
+    "revision": "79d121a0a2a120f4a502",
+    "url": "/static/js/2.f86cbe30.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.f86cbe30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8e805d531cc4cc85c55",
+    "url": "/static/js/main.bdc212af.chunk.js"
+  },
+  {
+    "revision": "284d5da4326391255d0b",
+    "url": "/static/js/runtime-main.d55536e7.js"
+  }
+]);
