0aa3d1ca208d2feb0ffe9346faf68d91d28bdc87
Gleb <glebslivko777@gmail.com>
2020-03-17 16:35:33 +0200

    npm run build

---
+++ simple-blog/dist/precache-manifest.05b8f5e189a152714fdc36d85f9e0f55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc76df64db8da9fc28179e98c10f6c8",
+    "url": "/DevelopsToday/simple-blog/dist/index.html"
+  },
+  {
+    "revision": "dc27ca87aa78c8491c61",
+    "url": "/DevelopsToday/simple-blog/dist/static/css/main.08b5e105.chunk.css"
+  },
+  {
+    "revision": "79d121a0a2a120f4a502",
+    "url": "/DevelopsToday/simple-blog/dist/static/js/2.f86cbe30.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/DevelopsToday/simple-blog/dist/static/js/2.f86cbe30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc27ca87aa78c8491c61",
+    "url": "/DevelopsToday/simple-blog/dist/static/js/main.926760d2.chunk.js"
+  },
+  {
+    "revision": "016a2ccf952fa205880a",
+    "url": "/DevelopsToday/simple-blog/dist/static/js/runtime-main.c0c4f9af.js"
+  }
+]);
