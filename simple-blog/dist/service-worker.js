d72613d7d3132000a8c60613914b7994f2396a26
Gleb <glebslivko777@gmail.com>
2020-03-17 16:03:40 +0200

    deploy

--- simple-blog/dist/service-worker.js
+++ simple-blog/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.09b249ce678dc76b0b76235a8ba72259.js"
+  "/DevelopsToday/simple-blog/dist/precache-manifest.a9de41165ed4097f766a84ad235d3aa0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DevelopsToday/simple-blog/dist/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
